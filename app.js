656e81e63e0099cf62a1cb118596ea02be20d049
Muhammad Bilal <mohammadbilal0345@gmail.com>
2020-08-26 18:24:34 +0500

    ready to post

---
+++ app.js
@@ -0,0 +1,21 @@
+
+//  let a;
+
+// function Show_hide(){
+
+
+//     if(a == 1){
+//         document.getElementById("my-div").style.display="none";
+//         return a=0;
+        
+//     }
+//     else{
+//         document.getElementById("my-div").style.display="block";
+       
+//         return a=1;
+        
+
+//     }
+// }
+
+alert("This WebSite Is Created By Muhammad Bilal");
